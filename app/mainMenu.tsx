dd7308fbab232eaad7c32740982eacff4e409f1a
Ubaldo Godinez <ubgodinez@gmail.com>
2024-01-17 14:09:44 -0600

    Beatles_App_2: Create main page.

---
+++ app/mainMenu.tsx
@@ -0,0 +1,12 @@
+export default function MainMenu() {
+    return (
+        <div>
+            <ul>
+                <li>John Lennon</li>
+                <li>Paul McCartney</li>
+                <li>George Harrison</li>
+                <li>Ringo Starr (OK, He is a drummer)</li>
+            </ul>
+        </div>
+    );
+}
